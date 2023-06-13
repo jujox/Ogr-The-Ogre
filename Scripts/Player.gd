35e49d005630ddc13deb5eacd5f04ad4b365fa53
Juanjo Escribano <juanjo.escribano@gmail.com>
2023-06-13 22:07:30 +0200

    Level 1: using CharacterBody2d for Player

---
+++ Scripts/Player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+
+func animate_sprite():
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite2D.play("right")
+	elif Input.is_action_pressed("move_left"):
+		$AnimatedSprite2D.play("left")
+	elif Input.is_action_pressed("move_down"):
+		$AnimatedSprite2D.play("down")
+	elif Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.play("up")
+	else:
+		$AnimatedSprite2D.stop()	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	animate_sprite()
+	get_input()
+	move_and_collide(velocity * delta)
