2f88bfe670cd96325f306f1754524c605db6613a
Juanjo Escribano <juanjo.escribano@gmail.com>
2023-06-18 13:02:43 +0200

    Changing enemy type to area2d

--- Level-1/Enemy.gd
+++ Level-1/Enemy.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var SPEED = 200
 var MOVEMENT_LIMIT = (randi() % 30) + 20
@@ -6,13 +6,17 @@ var UP_MOVEMENT_LIMIT = MOVEMENT_LIMIT
 var DOWN_MOVEMENT_LIMIT = -MOVEMENT_LIMIT
 var direction_y = 0
 var actual_direction = Vector2.UP
+var screen_size
+
+func _ready():
+	screen_size = get_parent().get_viewport_rect().size
 
 func reset_movement_limits():
 	MOVEMENT_LIMIT = (randi() % 30) + 20
 	UP_MOVEMENT_LIMIT = MOVEMENT_LIMIT
 	DOWN_MOVEMENT_LIMIT = -MOVEMENT_LIMIT
 	
-func _physics_process(delta):
+func _process(delta):
 	if actual_direction == Vector2.UP  and direction_y <= UP_MOVEMENT_LIMIT:
 		direction_y += 1
 	elif actual_direction == Vector2.UP:
@@ -26,11 +30,16 @@ func _physics_process(delta):
 		actual_direction = Vector2.UP
 		direction_y += 1
 
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		var collider = collision.get_collider()
-		if collider.name.begins_with("Player"):
-			collider.get_node("AnimatedSprite2D").play("hurted_ogre")
+	position.y += actual_direction.y * SPEED * delta
+	position.y = clamp(position.y, 0, screen_size.y)
 
-	velocity = actual_direction * SPEED
 
+func _on_body_entered(body):
+	if body.name.begins_with("Player"):
+		body.hurted = true
+
+
+
+func _on_body_exited(body):
+	if body.name.begins_with("Player"):
+		body.hurted = false
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
 
 @export var speed = 100
+var hurted = false
 
 func get_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
 
+func get_sprite_movement(sprite: String):
+	if hurted:
+		return "hurted_" + sprite
+	else:
+		return sprite
+
 func animate_sprite():
 	if Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.play("right")
+		$AnimatedSprite2D.play(get_sprite_movement("right"))
 	elif Input.is_action_pressed("move_left"):
-		$AnimatedSprite2D.play("left")
+		$AnimatedSprite2D.play(get_sprite_movement("left"))
 	elif Input.is_action_pressed("move_down"):
-		$AnimatedSprite2D.play("down")
+		$AnimatedSprite2D.play(get_sprite_movement("down"))
 	elif Input.is_action_pressed("move_up"):
-		$AnimatedSprite2D.play("up")
+		$AnimatedSprite2D.play(get_sprite_movement("up"))
 	else:
-		$AnimatedSprite2D.stop()	
+		if hurted:
+			$AnimatedSprite2D.play("hurted_ogre")
+		else:
+			$AnimatedSprite2D.stop()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
